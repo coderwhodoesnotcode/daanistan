d612f2066b2143e3e83d5a357618d7634404b725
coderwhodoesnotcode <fawadtwopointo@gmail.com>
2025-12-22 12:06:39 +0500

    added redirection

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/assistant-manager-chemistry-awc-test-preparation',
+        destination: '/awc-test-preparation',
+        permanent: true, // 301 redirect (SEO friendly)
+      },
+    ]
+  },
 };
-
-export default nextConfig;
