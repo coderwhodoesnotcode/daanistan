c551055edaefc9de1dc18b4082a06e488e2b58ba
coderwhodoesnotcode <fawadtwopointo@gmail.com>
2025-12-30 12:52:09 +0500

    mcqs

--- src/app/kpk-boards/[class]/[subject]/[chapter]/page.tsx
+++ src/app/kpk-boards/[class]/[subject]/[chapter]/page.tsx
@@ -123,14 +123,14 @@ export default function ChapterPage() {
     },
     "10": {
       "1": { slo: true, mcqs: true, numericals: false, notes: true },
-      "2": { slo: true, mcqs: true, numericals: true, notes: true },
-      "3": { slo: true, mcqs: true, numericals: true, notes: true },
+      "2": { slo: true, mcqs: true, numericals: false, notes: true },
+      "3": { slo: true, mcqs: true, numericals: false, notes: true },
       "4": { slo: true, mcqs: true, numericals: false, notes: true },
-      "5": { slo: true, mcqs: true, numericals: true, notes: true },
+      "5": { slo: true, mcqs: true, numericals: false, notes: true },
       "6": { slo: true, mcqs: true, numericals: false, notes: true },
-      "7": { slo: true, mcqs: true, numericals: true, notes: true },
+      "7": { slo: true, mcqs: true, numericals: false, notes: true },
       "8": { slo: true, mcqs: true, numericals: false, notes: true },
-      "9": { slo: true, mcqs: true, numericals: true, notes: true }
+      // "9": { slo: true, mcqs: true, numericals: true, notes: true }
     },
     "11": {
       "1": { slo: true, mcqs: true, numericals: false, notes: true },
